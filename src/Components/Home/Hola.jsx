1ece80c8d695c59e3705b3a57ac65add1c78eb2c
pedro-leoni <pedroleoni1996@gmail.com>
2022-07-03 19:15:46 -0300

    texto de maquina funcionando en Hola.jsx

--- src/Components/Home/Hola.jsx
+++ src/Components/Home/Hola.jsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React from "react";
 import { Box, Heading, useBreakpointValue } from "@chakra-ui/react";
-import DescargarCv from '../Buttons/DescargarCv';
-import SobreMiButton from '../Buttons/SobreMiButton';
-
+import DescargarCv from "../Buttons/DescargarCv";
+import SobreMiButton from "../Buttons/SobreMiButton";
+import Typewriter from "typewriter-effect";
 
 const Hola = () => {
-  const prueba = useBreakpointValue({base:{size:'4xl',color:'red'}, md:{size:'2xl',color:'green'}})
+  const machineText = (typewriter) => {
+    typewriter.typeString("Hola, soy Pedro Leoni.").start();
+  };
+  const machineText2 = (typewriter) => {
+    typewriter
+      .pauseFor(5000)
+      .typeString("Full Stack Web Developer")
+      .pauseFor(2000)
+      .deleteAll()
+      .typeString("Frontend Developer")
+      .pauseFor(2000)
+      .deleteAll()
+      .typeString("Backend Developer")
+      .pauseFor(2000)
+      .start();
+  };
+
   return (
-    <Box textAlign='center' pt='33vh'>
-      <Box variant={prueba}>
-        <Heading as='h1' size={'4xl'}  color='textColor' noOfLines={1} m={3} p={3} >
-          Hola, soy Pedro Leoni
+    <Box textAlign="center" pt="33vh">
+      <Box>
+        <Heading
+          as="h1"
+          size={["", "1xl", "2xl"]}
+          color="textSecondary"
+          noOfLines={1}
+          m={3}
+          p={3}
+        >
+          <Typewriter onInit={machineText} />
+          {/* Full Stack Web Developer */}
         </Heading>
-        <Heading  as='h1' size={['','2xl','2xl']} color='textSecondary' noOfLines={1} m={3} p={3} >
-          Full Stack Web Developer
+
+        <Heading
+          as="h1"
+          size={["","2xl","4xl"]}
+          color="textColor"
+          noOfLines={1}
+          m={3}
+          p={3}
+        >
+          <Typewriter onInit={machineText2} options={{ loop: true }} />
+          {/* Hola, soy Pedro Leoni */}
         </Heading>
       </Box>
       <Box>
         <SobreMiButton />
-        <DescargarCv/>
+        <DescargarCv />
       </Box>
     </Box>
-  ) 
-}
+  );
+};
 
-export default Hola
+export default Hola;
